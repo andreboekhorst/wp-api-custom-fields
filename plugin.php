cbb71b9b911ef87dcd79c7c7ab6333b8062e9d98
Andre Boekhorst <freelance@andreboekhorst.nl>
2015-04-24 21:23:09 +0200

    Start

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WP REST API - Advanced Custom Fields
+ * Description: Adds meta fields to WP Rest JSON output
+ * Version: 0.1
+ * Author: 
+ * Author URI: http://www.andreboekhorst.nl
+ * Plugin URI: https://github.com/WP-API/WP-API
+ */
+
